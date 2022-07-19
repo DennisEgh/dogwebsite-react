554293d7ae2e229932f16fdabc017dac6d5b6f26
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-19 22:27:25 +0100

    added beginning of skeleton

--- src/subcomponents/Gallery.jsx
+++ src/subcomponents/Gallery.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
+import React, { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
 import "../index.css";
-import 'react-alice-carousel/lib/alice-carousel.css';
-import Img1 from '../assets/logo.png'
-import Img2 from '../assets/playing.jpg'
-import Img3 from '../assets/dogs2.jpg'
+import "react-alice-carousel/lib/alice-carousel.css";
+import Img1 from "../assets/logo.png";
+import Img2 from "../assets/playing.jpg";
+import Img3 from "../assets/dogs2.jpg";
 
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-  <img src={Img1} className="sliderimg" onDragStart={handleDragStart} role="presentation" />,
-  <img src={Img2} className="sliderimg" onDragStart={handleDragStart} role="presentation" />,
-  <img src={Img3} className="sliderimg" onDragStart={handleDragStart} role="presentation" />,
+  <div className="item" data-value="1">
+    <img className="item--second" src={Img2} alt="" />
+    <h1 className="item--third">Hello</h1>
+  </div>,
+
+  <div className="item" data-value="2">
+    <img className="item--second" src={Img2} alt="" />
+  </div>,
+
+  <div className="item" data-value="3">
+    <img className="item--second" src={Img2} alt="" />
+  </div>,
 ];
 
 const Gallery = () => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = Img2;
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, 300);
+    };
+  });
+
   return (
-    <AliceCarousel mouseTracking items={items} />
+    <>
+      {img ? (
+        <>
+          <AliceCarousel mouseTracking items={items} />
+        </>
+      ) : (
+        <> 
+         <div className="skeleton"></div>
+        </>
+      )}
+     
+    </>
   );
-}
-export default Gallery
+};
+export default Gallery;
