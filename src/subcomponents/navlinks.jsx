ece5d8d34363f84099285ee2855c03fcdeb424aa
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-21 23:16:00 +0100

    fixed small things

--- src/subcomponents/navlinks.jsx
+++ src/subcomponents/navlinks.jsx
@@ -8,12 +8,15 @@ const Navlinks = () => {
       <div className="nav__links--container">
         <div className="nav__links">
           <ul className="nav__link--list">
-            <li className="nav__link">
+            <div className="nav__link--wrapper">
+
+            <li className="nav__link adopt">
               <Link to="/" className="nav__link--anchor">
                 ADOPT
               </Link>
             </li>
-            <li className="nav__link">
+
+            <li className="nav__link veterinary">
               <Link to="/" className="nav__link--anchor">
                 VETERINARY CARE
               </Link>
@@ -23,13 +26,14 @@ const Navlinks = () => {
                 PROGRAMS & RESOURCES
               </Link>
             </li>
+            </div>
+            <div className="shop__container">
+              <div className="shop__content">
+                <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+                <p className="shop__para">SHOP</p>
+              </div>
+            </div>
           </ul>
-        </div>
-      </div>
-      <div className="shop__container">
-        <div className="shop__content">
-          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-          <p className="shop__para">SHOP</p>
         </div>
       </div>
     </div>
