d337859f99d667a5458aa67cb81c484a3f085e02
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-22 14:55:17 +0100

    added modal

---
+++ src/subcomponents/Modal.jsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+function Modal() {
+  return (
+    <div className="modal__wrapper">
+      <div className="modal__container">
+        <div className="modal__header">
+          <h1 className="modal__title">LOGIN</h1>
+          <FontAwesomeIcon
+            onClick={() => document.body.classList.remove("menu--open")}
+            icon="fa-solid fa-xmark"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Modal;
