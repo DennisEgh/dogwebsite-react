0fd72112036f4dd7654a866a73e25464de56e850
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-22 14:55:10 +0100

    added sign in modal

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,85 +3,93 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import Navlinks from "../subcomponents/navlinks";
-
+import Modal from "../subcomponents/Modal";
 
 
 const Nav = () => {
-
-
-function openSearch(){
-  let element = document.querySelector(".interaction__container")
-  element.classList.toggle("search__active");
-}
-
-const searching = () =>{
-
-  let input = document.querySelector(".search__input")
-  
-  return input.value
-}
-
-const handleKeyPress = (event) =>{
-  if(event.key === 'Enter'){
-    console.log(searching())
+  function openSearch() {
+    let element = document.querySelector(".interaction__container");
+    element.classList.toggle("search__active");
   }
-}
 
+    function openLogIn() {
+    document.body.classList.toggle("menu--open");
+  }
 
+  const searching = () => {
+    let input = document.querySelector(".search__input");
 
+    return input.value;
+  };
 
-
-
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      console.log(searching());
+    }
+  };
 
   return (
-    <nav>
-      <div className="nav__container--upper">
-        <Link to="/">
-          <div className="nav__logo--container">
-            <figure className="nav__logo">
-              <img className="logo" src={Logo} alt="" />
-            </figure>
-            <h1 className="nav__logo--title">PawMate</h1>
-          </div>
-        </Link>
-
-        <ul className="interaction__container">
-          <li className="interaction">
-            <p className="search__para">HOW TO HELP</p>
-          </li>
-          <li className="interaction">
-            <p className="search__para">REPORT CRUELTY</p>
-          </li>
-      
-          <li className="interaction" id="search">
-            <div className="interaction__container--search">
-
-            <FontAwesomeIcon onClick={openSearch} icon="fa-solid fa-magnifying-glass" />
-            <p className="search__para" onClick={openSearch}>SEARCH</p>
-            
+    <>
+      <nav>
+        <div className="nav__container--upper">
+          <Link to="/">
+            <div className="nav__logo--container">
+              <figure className="nav__logo">
+                <img className="logo" src={Logo} alt="" />
+              </figure>
+              <h1 className="nav__logo--title">PawMate</h1>
             </div>
-            
-            
+          </Link>
 
-            
-            <FontAwesomeIcon onClick={openSearch} icon="fa-solid fa-circle-xmark" />
-            <input onKeyPress={handleKeyPress} className="search__input" type="text" placeholder="What are you looking for?"
-            />
-            
-            
-          </li>
-          <li className="interaction">
-            <div className="interaction__container--user">
+          <ul className="interaction__container">
+            <li className="interaction">
+              <p className="search__para">HOW TO HELP</p>
+            </li>
+            <li className="interaction">
+              <p className="search__para">REPORT CRUELTY</p>
+            </li>
 
-          <FontAwesomeIcon icon="fa-solid fa-circle-user" />
-            <p className="search__para">LOG IN</p>
-            </div>
-          </li>
-        </ul>
-      </div>
+            <li className="interaction" id="search">
+              <div className="interaction__container--search">
+                <FontAwesomeIcon
+                  onClick={openSearch}
+                  icon="fa-solid fa-magnifying-glass"
+                />
+                <p className="search__para" onClick={openSearch}>
+                  SEARCH
+                </p>
+              </div>
 
-      <Navlinks></Navlinks>
-    </nav>
+              <FontAwesomeIcon
+                onClick={openSearch}
+                icon="fa-solid fa-circle-xmark"
+              />
+              <input
+                onKeyPress={handleKeyPress}
+                className="search__input"
+                type="text"
+                placeholder="What are you looking for?"
+              />
+            </li>
+            <li className="interaction">
+              <div className="interaction__container--user">
+                <FontAwesomeIcon
+                  onClick={openLogIn}
+                  icon="fa-solid fa-circle-user"
+                />
+                <p onClick={openLogIn} className="search__para">
+                  LOG IN
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <Navlinks></Navlinks>
+        <Modal></Modal>
+      </nav>
+      <div className="nav__bg"></div>
+    </>
   );
 };
 export default Nav;
+
