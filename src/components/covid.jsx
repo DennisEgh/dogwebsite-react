f8a5249c9af76d7ca83d882499c31b50d0b6f2e9
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-22 22:21:15 +0100

    added covid to landingpage

---
+++ src/components/covid.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Covidlogo from "../assets/covid.png";
+
+function Covid() {
+  return (
+    <div className="covid__container">
+      <div className="covid__wrapper">
+        <div className="covid__inner">
+          <div className="covid__header">
+            <div className="covid__icon">
+              <img src={Covidlogo} alt="" className="icon" />
+            </div>
+            <header className="covid__title">COVID-19 Information</header>
+          </div>
+          <div className="covid__content">
+            <p className="covid__para">
+              Learn more about our efforts to prevent the <br />
+              spread of Coronavirus.
+            </p>
+            <Link className="covid__anchor" to="/">
+              <div className="covid__btn">Learn More</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Covid;
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,15 +1,11 @@
-import React from "react"
+import React from "react";
 import Gallery from "../subcomponents/Gallery";
 
-
-
-const Landing = () =>{
-    return(
-<section id="landing-page">
-
-    <Gallery></Gallery>
-    
-</section>
-    )
-}
+const Landing = () => {
+  return (
+    <section id="landing-page">
+      <Gallery></Gallery>
+    </section>
+  );
+};
 export default Landing;
