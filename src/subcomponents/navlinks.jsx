ca174bcf071e937bc8050fedd4b44c985b9e4158
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-25 19:23:50 +0100

    öamldöadmöad

--- src/subcomponents/navlinks.jsx
+++ src/subcomponents/navlinks.jsx
@@ -15,7 +15,59 @@ const Navlinks = () => {
                   <div className="nav__link--square adopt__square"></div>
                 </Link>
 
-                <div className="nav__link--dropdown adopt__dropdown"></div>
+                <div className="nav__link--dropdown adopt__dropdown">
+                  <div className="columns">
+                    <div className="column__container">
+                      <div className="column column__one">
+                        <h1 className="column__title borderbot">
+                          ALL ADOPTION CENTERS
+                        </h1>
+                        <ul className="column__one--lists">
+                          <li className="column__one--list borderbot">
+                            <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+                            <Link to="/" className="column__one--anchor">
+                              Texas
+                            </Link>
+                          </li>
+                          <li className="column__one--list borderbot">
+                            <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+                            <Link to="/" className="column__one--anchor">
+                              New York
+                            </Link>
+                          </li>
+                          <li className="column__one--list borderbot">
+                            <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+                            <Link to="/" className="column__one--anchor">
+                              Washington DC
+                            </Link>
+                          </li>
+                          <li className="column__one--list">
+                            <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+                            <Link to="/" className="column__one--anchor">
+                              Lost or Found a Pet?
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+
+                    <div className="column__container">
+                      <div className="column column__two">
+                        <h1 className="column__title">SEARCH ADOPTABLE DOGS</h1>
+                      </div>
+                    </div>
+                    <div className="column__container">
+                      <div className="column column__three">
+                        <h1 className="column__title">DOGVOCATES</h1>
+                      </div>
+                    </div>
+                    <div className="column__container">
+                      <div className="column column__four">
+                        <h1 className="column__title">WISHLIST</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </li>
 
               <li className="nav__link veterinary vet">
@@ -33,12 +85,8 @@ const Navlinks = () => {
                   <div className="nav__link--square pro-res__square"></div>
                 </Link>
 
-                
-                <div className="nav__link--dropdown pro-res__dropdown">
-                  
-                </div>
+                <div className="nav__link--dropdown pro-res__dropdown"></div>
               </li>
-
             </div>
             <Link className="shop__anchor" to="/store">
               <div className="shop__container">
