81788d17f1548ae13207db1b5ae45eaaf10068d5
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-25 17:28:41 +0100

    added dropdown to nav__links

--- src/subcomponents/navlinks.jsx
+++ src/subcomponents/navlinks.jsx
@@ -9,32 +9,44 @@ const Navlinks = () => {
         <div className="nav__links">
           <ul className="nav__link--list">
             <div className="nav__link--wrapper">
+              <li className="nav__link adopt">
+                <Link to="/" className="nav__link--anchor">
+                  ADOPT
+                  <div className="nav__link--square adopt__square"></div>
+                </Link>
 
-            <li className="nav__link adopt">
-              <Link to="/" className="nav__link--anchor">
-                ADOPT
-              </Link>
-            </li>
+                <div className="nav__link--dropdown adopt__dropdown"></div>
+              </li>
 
-            <li className="nav__link veterinary">
-              <Link to="/" className="nav__link--anchor">
-                VETERINARY CARE
-              </Link>
-            </li>
-            <li className="nav__link">
-              <Link to="/" className="nav__link--anchor">
-                PROGRAMS & RESOURCES
-              </Link>
-            </li>
+              <li className="nav__link veterinary vet">
+                <Link to="/" className="nav__link--anchor">
+                  VETERINARY CARE
+                  <div className="nav__link--square vet__square"></div>
+                </Link>
+
+                <div className="nav__link--dropdown vet__dropdown"></div>
+              </li>
+
+              <li className="nav__link pro-res">
+                <Link to="/" className="nav__link--anchor pro-res">
+                  PROGRAMS & RESOURCES
+                  <div className="nav__link--square pro-res__square"></div>
+                </Link>
+
+                
+                <div className="nav__link--dropdown pro-res__dropdown">
+                  
+                </div>
+              </li>
+
             </div>
             <Link className="shop__anchor" to="/store">
-
-            <div className="shop__container">
-              <div className="shop__content">
-                <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-                <p className="shop__para">SHOP</p>
+              <div className="shop__container">
+                <div className="shop__content">
+                  <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+                  <p className="shop__para">SHOP</p>
+                </div>
               </div>
-            </div>
             </Link>
           </ul>
         </div>
