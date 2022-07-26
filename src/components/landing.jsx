76e4568a93d3e57fb35d84af04a5213e74be3216
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-26 13:57:25 +0100

    pdapda

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Gallery from "../subcomponents/Gallery";
+import Covid from '../components/covid';
 
 const Landing = () => {
   return (
     <section id="landing-page">
       <Gallery></Gallery>
+      <Covid></Covid>
     </section>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Covid from '../components/covid';
+
 import Landing from '../components/landing';
 
 const Home = () => {
     return(
         <>
      <Landing></Landing>
-     <Covid></Covid>
+     
         </>
     )
 }
