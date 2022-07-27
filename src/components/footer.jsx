66545e6c48b485da36c2c9699eb5818e1f984c4c
Dennis Eghtedari <year_2000_@hotmail.com>
2022-07-27 17:05:22 +0100

    removed fontawesome no unused vars

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import facebook from "../assets/facebook.png"
